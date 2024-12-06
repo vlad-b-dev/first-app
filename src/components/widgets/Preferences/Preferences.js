2e52a5d8678fcf3e927cf909925c2f9cb10219d3
Vladyslav Boychuk <vboychuk1122@gmail.com>
2024-12-06 17:25:06 +0100

    - Preferences button developed and added animations

--- src/components/widgets/Preferences/Preferences.js
+++ src/components/widgets/Preferences/Preferences.js
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
 import ThemeSwitch from "../ThemeSwitch/ThemeSwitch";
+import preferencesIcon from "../../../resources/images/icons/optionsGear.png";
+import preferencesIconHover from "../../../resources/images/icons/optionsGearHover.png";
 import LanguageSelect from "../LanguageSelect/LanguageSelect";
 import { Button } from "@mui/material";
 
+import "./Preferences.scss";
+
 const UserSettings = () => {
   const [showPreferences, setShowPreferences] = useState(false);
+  const [hover, setHover] = useState(false);
 
   const preferencesClick = () => {
     setShowPreferences(!showPreferences);
@@ -18,10 +23,19 @@ const UserSettings = () => {
           fontSize: "20px",
           whiteSpace: "nowrap",
           color: "var(--main-purple-color)",
+          backgroundColor: "transparent",
+          "&:hover": {
+            backgroundColor: "transparent",
+          },
         }}
       >
-        {/* TODO add icon instead of text */}
-        {"PREFERENCES"}
+        <img
+          src={hover ? preferencesIconHover : preferencesIcon}
+          onMouseEnter={() => setHover(true)}
+          onMouseLeave={() => setHover(false)}
+          className="preferences-gear"
+          alt="Logo"
+        />
       </Button>
       {showPreferences && (
         <div>
