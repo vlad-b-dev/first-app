01e1b5cc7d104826972aa3fbcb7251a6b0f88b2c
Vladyslav Boychuk <vboychuk1122@gmail.com>
2024-12-06 17:54:35 +0100

    - Improved some preferences details

--- src/components/widgets/Preferences/Preferences.js
+++ src/components/widgets/Preferences/Preferences.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import ThemeSwitch from "../ThemeSwitch/ThemeSwitch";
-import preferencesIcon from "../../../resources/images/icons/optionsGear.png";
-import preferencesIconHover from "../../../resources/images/icons/optionsGearHover.png";
+import preferencesIcon from "../../../resources/images/icons/preferencesGear/preferencesGear.png";
+import preferencesIconHover from "../../../resources/images/icons/preferencesGear/preferencesGearHover.png";
 import LanguageSelect from "../LanguageSelect/LanguageSelect";
 import { Button } from "@mui/material";
 
@@ -18,25 +18,35 @@ const UserSettings = () => {
   return (
     <div>
       <Button
-        onClick={preferencesClick}
-        sx={{
-          fontSize: "20px",
-          whiteSpace: "nowrap",
-          color: "var(--main-purple-color)",
-          backgroundColor: "transparent",
-          "&:hover": {
-            backgroundColor: "transparent",
-          },
-        }}
-      >
-        <img
-          src={hover ? preferencesIconHover : preferencesIcon}
-          onMouseEnter={() => setHover(true)}
-          onMouseLeave={() => setHover(false)}
-          className="preferences-gear"
-          alt="Logo"
-        />
-      </Button>
+  onClick={preferencesClick}
+  sx={{
+    fontSize: "20px",
+    whiteSpace: "nowrap",
+    color: "var(--main-purple-color)",
+    backgroundColor: "transparent",
+    "&:hover": {
+      backgroundColor: "transparent",
+    },
+  }}
+>
+  <div 
+    className="preferences-gear-wrapper"
+    onMouseEnter={() => setHover(true)}
+    onMouseLeave={() => setHover(false)}
+  >
+    <img
+      src={preferencesIcon}
+      className={`preferences-gear ${hover ? "hidden" : "visible"}`}
+      alt="Default Gear"
+    />
+    <img
+      src={preferencesIconHover}
+      className={`preferences-gear ${hover ? "visible" : "hidden"}`}
+      alt="Hover Gear"
+    />
+  </div>
+</Button>
+
       {showPreferences && (
         <div>
           <ThemeSwitch />
