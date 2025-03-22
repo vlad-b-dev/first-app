4919c1df4a9009d93daee3e97b10fc5bbdbdbc4d
Vladyslav Boychuk <vboychuk1122@gmail.com>
2025-03-22 21:02:48 +0100

    - Updated personal section

--- src/components/views/Resume/PersonalSection/PersonalSection.js
+++ src/components/views/Resume/PersonalSection/PersonalSection.js
@@ -2,6 +2,11 @@ import React from "react";
 import PropTypes from "prop-types";
 import ContentSection from "../../../ui/sections/ContentSection/ContentSection";
 import { useTranslation } from "react-i18next";
+import GenericLogo from "../../../ui/mediaViewers/GenericLogo/GenericLogo";
+import EuropeMapLight from "../../../../resources/images/contentPhotos/personal/europeMap/europeMapLight.webp";
+import EuropeMapDark from "../../../../resources/images/contentPhotos/personal/europeMap/europeMapDark.webp";
+import Pets from "../../../../resources/images/contentPhotos/personal/pets/pets.webp";
+import Stuff from "../../../../resources/images/contentPhotos/personal/stuff/stuff.webp";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -15,49 +20,57 @@ const PersonalSection = () => {
       startExpanded={false}
       title={t("resumePage.personalSectionTitle")}
     >
-      Esta sección es más personal, no esta de mas conocer un poco con quien vas
-      a pasar unas 40h a la semana.
-      <br />
-      <br />
-      Nací en Ucrania y me crié en un pueblo del sur de Navarra, aunque
-      actualmente convivo con un gecko leopardo (Menta ) y una docena de
-      pececillos amazónicos en Pamplona. Estos cambios me han permitido tener
-      amistades de distintos países, culturas, edades y ambientes... Lo
-      importante es la calidad, y en eso he tenido mucha suerte.
-      <br />
-      <br />
-      Soy una persona con dos facetas: una intensa, despreocupada y enérgica,
-      otra responsable, comprometida y profesional.
-      <br />
-      <br />
-      Me gusta lo tradicional y también lo moderno, lo natural y lo urbano, lo
-      tranquilo y lo intenso, lo exacto y lo creativo... ¿Bangkok o los
-      Pirineos? ¿Programación o diseño? ¿En casa con los colegas o de fiesta a
-      Budapest? ¿Meditación o motocross?
-      <br />
-      <br />
-      No lo sé, pero está claro que el balance es importante, aunque sea en los
-      extremos.
-      <br />
-      <br />
-      Siempre tengo algun proyecto independiente a mi carrera profesional:
-      desarrollo, diseño, reparaciones, botánica, impresión 3D, pintura...
-      Crecer y aprender es vital, pero lo que más me apasiona es el mundo del
-      motor: mecánica, coches, motos...
-      <br />
-      <br />
-      Desde pequeño he tenido una manera muy única de hacer las cosas. Esto no
-      siempre resulta tan bueno como muchos creen, sobre todo hasta que aprendes
-      a sacarle provecho.
-      <br />
-      <br />
-      He tenido temporadas muy buenas y otras no tan buenas, pero he aprendido
-      que hay que intentar dar el 100% en cada ocasión y no perder la pasión.
-      Solo se vive una vez.
-      <br />
-      <br />
-      Pero mejor hablamos de estos temas con un par de cervezas tras alguna cena
-      corporativa; el networking es importante.
+      <div className="row mt-3">
+        Esta sección es más personal, no esta de mas conocer un poco con quien
+        vas a pasar unas 40h a la semana. Nacido en Ucrania y me ciado en un
+        pueblo del sur de Navarra, actualmente convivo con mi gecko leopardo
+        (Menta) y una docena de peces amazónicos en Pamplona.
+      </div>
+      <div className="row mt-4">
+        <div className="col-6">
+          <GenericLogo logo={Pets} width={"45vw"} hoverScale={1} />
+        </div>
+        <div className="col-6 mt-1 text-end">
+          <GenericLogo
+            className="mt-4 mr-0 pr-0"
+            logoDark={EuropeMapDark}
+            logoLight={EuropeMapLight}
+            width={"40vw"}
+            hoverScale={1}
+          />
+        </div>
+      </div>
+      <div className="row mt-4">
+        <div className="col-6 text-start">
+          Tengo dos partes: una intensa, despreocupada y enérgica, otra
+          responsable, comprometida y profesional. Lo importante es saber cuál
+          toca sacar en cada momento.
+          <br />
+          <br />
+          Al mismo tiempo gusta lo tradicional y también lo moderno, lo natural
+          y lo urbano, lo tranquilo y lo intenso, lo exacto y lo creativo...
+          ¿Bangkok o los Pirineos? ¿Programación o diseño? ¿En casa con los
+          colegas o de fiesta a Budapest? ¿Meditación o motocross?
+          <br />
+          Siempre hay algun proyecto para hacer: desarrollo, diseño,
+          reparaciones, botánica, impresión 3D, pintura... Crecer y aprender es
+          importante. Mención especial al mundo del motor: mecánica, coches,
+          motos...
+          <br />
+          <br />
+          He tenido una manera muy única de hacer las cosas, y aprendí a sacarle
+          provecho. Todos tenemos temporadas buenas y otras no tan buenas, pero
+          nunca hay que perder la perspectiva y pensar a lo grande. Solo se vive
+          una vez.
+          <br />
+          <br />
+          Podemos hablar de otros temas más filosóficos con un par de cervezas
+          tras alguna cena corporativa; el networking es importante.
+        </div>
+        <div className="col-6 text-center">
+          <GenericLogo logo={Stuff} width={"37vw"} hoverScale={1} />
+        </div>
+      </div>
     </ContentSection>
   );
 };
