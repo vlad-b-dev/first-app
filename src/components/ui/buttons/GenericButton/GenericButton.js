5b6e532b6f97717c3aa8da0121c6dfdb39a2f396
Vladyslav Boychuk <vboychuk1122@gmail.com>
2025-03-29 20:12:57 +0100

    - Finished ahead section - Added content to website - Some corrections

--- src/components/ui/buttons/GenericButton/GenericButton.js
+++ src/components/ui/buttons/GenericButton/GenericButton.js
@@ -2,17 +2,23 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./GenericButton.scss";
 
-const GenericButton = ({ label, onClick, className = "" }) => {
+const GenericButton = ({ label, onClick, className = "", width = "100%" }) => {
   return (
-    <button className={`generic-button ${className}`} onClick={onClick}>
+    <button
+      className={`generic-button ${className}`}
+      onClick={onClick}
+      style={{ width }}
+    >
       {label}
     </button>
   );
 };
+
 GenericButton.propTypes = {
   label: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
   className: PropTypes.string,
+  width: PropTypes.string,
 };
 
 export default GenericButton;
