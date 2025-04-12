e0f4b9c889dab73b839dd4759e41f30f65a6fe08
Vladyslav Boychuk <vboychuk1122@gmail.com>
2025-04-12 20:03:07 +0200

    - Finished footer

--- src/components/ui/menus/MainFooter/FooterImage/FooterImage.js
+++ src/components/ui/menus/MainFooter/FooterImage/FooterImage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PamplonaSkylineDark from "../../../../../resources/images/backgrounds/footer/pamplonaSkyline/pamplonaSkylineDark.webp";
 import PamplonaSkylineLight from "../../../../../resources/images/backgrounds/footer/pamplonaSkyline/pamplonaSkylineLight.webp";
 import { useTheme } from "../../../../../styles/ThemeContext";
@@ -9,9 +9,34 @@ const FooterImage = () => {
   const PamplonaSkyline =
     theme === "dark" ? PamplonaSkylineDark : PamplonaSkylineLight;
 
+  const [gradientElement, setGradientElement] = useState(null);
+
+  useEffect(() => {
+    if (!gradientElement) return;
+
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+        } else {
+          entry.target.classList.remove("visible");
+        }
+      },
+      {
+        threshold: 0.5,
+      }
+    );
+
+    observer.observe(gradientElement);
+
+    return () => {
+      observer.unobserve(gradientElement);
+    };
+  }, [gradientElement]);
+
   return (
     <div className="footer-image-component-wrapper">
-      <div className="sunrise-gradient" />
+      <div ref={setGradientElement} className="sunrise-gradient" />
       <img src={PamplonaSkyline} alt="Skyline" className="footer-top-image" />
     </div>
   );
--- src/components/ui/menus/MainFooter/MainFooter.js
+++ src/components/ui/menus/MainFooter/MainFooter.js
@@ -1,20 +1,60 @@
 import React from "react";
 import FooterImage from "./FooterImage/FooterImage";
-/* import "./MainFooter.scss";
- */ const MainFooter = () => {
-  /*   const { t } = useTranslation();
-  const navigate = useNavigate();
-  const location = useLocation();
-  const { theme } = useTheme();
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= 767); */
+import FooterLogo from "../../../../resources/images/logos/footerLogo/footerLogo.webp";
+import ImageComponent from "../../mediaViewers/ImageComponent/ImageComponent";
+import LocalAirportTwoToneIcon from "@mui/icons-material/LocalAirportTwoTone";
+import "./MainFooter.scss";
 
-  /*   useEffect(() => {
-    const handleResize = () => setIsMobile(window.innerWidth <= 767);
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []); */
+const MainFooter = () => {
+  const scrollToTop = () => {
+    console.log(
+      "Scroll to top button clicked!",
+      document.querySelector(".page-background")
+    );
+    const scrollableContainer = document.querySelector(".page-background");
+    if (scrollableContainer) {
+      scrollableContainer.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    } else {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+  };
 
-  return <FooterImage />;
+  return (
+    <div className="footer-background">
+      <div className="footer-container">
+        <div className="footer-content">
+          <FooterImage />
+          <div className="footer-body">
+            <div className="row text-center">
+              <button onClick={scrollToTop} className="scroll-to-top-button">
+                <LocalAirportTwoToneIcon className="custom-airport-icon" />
+              </button>
+              <p>
+                Wow! You almost scrolled into the dark web — let me help you get
+                back!
+              </p>
+            </div>
+            <div className="row text-center">
+              <ImageComponent image={FooterLogo} hoverScale={1} width="15vw" />
+
+              <p>©Copyright by Owete | 2025 | All-In | All Rights Reserved</p>
+              <p>Eclater server version: 1.0.0-BETA</p>
+              <p>
+                All-In App | Designed, developed and deployed by Vladyslav
+                Boychuk
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MainFooter;
